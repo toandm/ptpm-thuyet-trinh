fec3fa0868aa2347a8e1080ebb646a1bca19cf3c
brian_toan_topdrill <brian.toan@topdrill.com.au>
2024-11-30 20:32:41 +0700

    add tuple

---
+++ 05_02_tuple_unpack.py
@@ -0,0 +1,18 @@
+print("\n")
+
+hs = ("Bình", 22, "Hà Nội")
+
+ten, tuoi, que_quan = hs
+
+print(f"Học sinh {ten}, {tuoi} tuổi, quê ở {que_quan}")
+
+# Bỏ qua 1 biến
+ten, _, que_quan = hs
+
+# Bỏ qua nhiều biến
+ten, *_ = hs
+
+# Dict items()
+d = {"a": 1}
+for i in d.items():
+    print(i)
